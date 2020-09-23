a43c1a6f018189bff89544116ce2872cff030885
Saša Tomić <sasa.tomic@dfinity.org>
2020-09-23 17:32:45 +0200

    Deployment to the Tungsten network now works! (#9)

--- bigmap_ui/bootstrap-ui.sh
+++ bigmap_ui/bootstrap-ui.sh
@@ -6,7 +6,20 @@ echo "Bootstraping BigMap..."
 # Support bootstrapping from any folder
 BASEDIR=$(cd "$(dirname "$0")"; pwd)
 
-"$BASEDIR/../bootstrap.sh"
+# "$BASEDIR/../bootstrap.sh" "$@"
+
+if [[ "$1" == "tungsten" ]]; then
+  echo "Bootstraping on the Tungsten network"
+  NETWORK="--network tungsten"
+  if [[ -z "$DFX_CREDS_USER" || -z "$DFX_CREDS_PASS" || -z "$DFX_NETWORK" ]]; then
+    echo "Please make sure you set the following environment variables:"
+    echo "export DFX_CREDS_USER={username}"
+    echo "export DFX_CREDS_PASS={password}"
+    echo "export DFX_NETWORK=tungsten"
+  fi
+else
+  echo "Bootstraping on the local instance"
+fi
 
 cd "$BASEDIR/.."
 address=$(dfx config defaults.start.address | tr -d '"')
@@ -17,18 +30,23 @@ cd "$BASEDIR"
 mkdir -p .dfx
 test -d ../.dfx/local && rsync -aP ../.dfx/local/ .dfx/local/
 test -d ../.dfx/tungsten && rsync -aP ../.dfx/tungsten/ .dfx/tungsten/
+test -f ../canister_ids.json && rsync -aP ../canister_ids.json ./
 
 echo "Bootstraping BigMap UI..."
 
 npm install
 
-echo "dfx build"
-dfx build
-echo "dfx canister install bigmap_ui --mode=reinstall"
-dfx canister install bigmap_ui --mode=reinstall
+echo "dfx build $NETWORK"
+dfx build $NETWORK
+echo "dfx canister $NETWORK install bigmap_ui --mode=reinstall"
+dfx canister $NETWORK install bigmap_ui --mode=reinstall
 
 
-URL="http://127.0.0.1:$port/?canisterId=$(dfx canister id bigmap_ui)"
+if [[ "$1" == "tungsten" ]]; then
+  URL="http://$(dfx canister $NETWORK id bigmap_ui).ic0.app/"
+else
+  URL="http://127.0.0.1:$port/?canisterId=$(dfx canister id bigmap_ui)"
+fi
 
 echo "Opening $URL"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,19 @@ set -e
 BASEDIR=$(cd "$(dirname "$0")"; pwd)
 cd "$BASEDIR"
 
+if [[ "$1" == "tungsten" ]]; then
+  echo "Bootstraping on the Tungsten network"
+  NETWORK="--network tungsten"
+  if [[ -z "$DFX_CREDS_USER" || -z "$DFX_CREDS_PASS" || -z "$DFX_NETWORK" ]]; then
+    echo "Please make sure you set the following environment variables:"
+    echo "export DFX_CREDS_USER={username}"
+    echo "export DFX_CREDS_PASS={password}"
+    echo "export DFX_NETWORK=tungsten"
+  fi
+else
+  echo "Bootstraping on the local instance"
+fi
+
 echo "Install dependencies"
 npm install
 
@@ -13,11 +26,11 @@ echo "Building canisters"
 # This command is ran from dfx build:
 # cargo build --release --target wasm32-unknown-unknown
 rm -rf canisters
-dfx canister create --all
-dfx build
+dfx canister $NETWORK create --all
+dfx build $NETWORK
 
 echo "Installing canisters"
-dfx canister install bigmap --mode=reinstall
+dfx canister $NETWORK install bigmap --mode=reinstall
 
 ./bigmap-cli --set-data-bucket-wasm-binary target/wasm32-unknown-unknown/release/bigmap_data.wasm
 ./bigmap-cli --set-search-wasm-binary target/wasm32-unknown-unknown/release/bigmap_search.wasm
